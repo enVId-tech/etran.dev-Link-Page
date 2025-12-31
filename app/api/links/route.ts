9458fcc3280801936949f15e865b29198f228dfa
Erick Tran <thelittlebotengineer@gmail.com>
2025-12-31 15:32:18 -0800

    feat(api): get links from links collection

---
+++ app/api/links/route.ts
@@ -0,0 +1,40 @@
+import { connectToDatabase } from "@/app/utils/db";
+import { NextResponse } from "next/server";
+
+export const revalidate = 1200;
+
+export async function GET() {
+    try {
+        const { db } = await connectToDatabase();
+        const collection = db.collection("links");
+
+        const links = await collection.find().toArray()
+
+        if (!links) {
+            return NextResponse.json(
+                {
+                    success: true,
+                    links: []
+                }
+            )
+        }
+
+        return NextResponse.json(
+            {
+                success: true,
+                links: links
+            },
+            {
+                headers: {
+                    'Cache-Control': 'public, s-maxage=1200, stale-while-revalidate=600',
+                },
+            }
+        )
+    } catch (err: unknown) {
+        console.error(`An error has occured fetching links: ${err as string}`)
+        return NextResponse.json(
+            { success: false, error: `Failed to fetch links: ${err as string}` },
+            { status: 500 }
+        )
+    }
+}
