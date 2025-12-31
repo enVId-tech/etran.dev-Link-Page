ec1a137b13d924da3c0cc45d1a5395053404b418
Erick Tran <thelittlebotengineer@gmail.com>
2025-12-31 00:09:30 -0800

    feat(layout.tsx): add proper metadata for error page

---
+++ app/error/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.scss";
+import icon from "@/public/envidtech.png";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Error 404 - Page Not Found",
+    description: "The requested page could not be found.",
+    icons: {
+        icon: icon.src
+    }
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                {children}
+            </body>
+        </html>
+    );
+}
